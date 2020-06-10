93b70ecbdec8f2e17c5ec8a9b9b575527e5c9166
danweaver12 <danweaver12@gmail.com>
2020-06-11 00:06:20 +0200

    added simple broadcasting and join/leave events

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server); // socket io requires passing the actual http server which is why we aren't letting express create it this time
 
 const port = process.env.PORT || 3000;
 const publicDicrectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDicrectoryPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New WebSocket connection');
+
+  socket.emit('message', 'Welcome!');
+
+  socket.broadcast.emit('message', 'A new user has joined!');
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left!');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
 });
